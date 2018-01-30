e89e5278d7cb3d6dbebe216a2aaf26a81ebed952
DavidGonzalezM <dg.martos@gmail.com>
2018-01-30 22:18:34 +0100

    IA de ultima generacion

---
+++ Assets/Scripts/EnemyBodyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBodyController : MonoBehaviour
+{
+
+    private Transform target;
+    public GameObject laserBullet;
+    private Transform gun1;
+    private Transform gun2;
+    public float shootTime = 2;
+    private bool shoot;
+
+	void Start ()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        shoot = true;
+        gun1 = transform.GetChild(0);
+        gun2 = transform.GetChild(1);
+    }
+	
+
+	void Update ()
+    {
+        transform.LookAt(target);
+        transform.Rotate(-90,-90,0);
+        if (shoot)
+        {
+            ShootLasers();
+            shoot = false;
+            Invoke("SetShootToTrue", shootTime);
+        }
+    }
+
+    void ShootLasers()
+    {
+        GameObject laser1 = Instantiate(laserBullet, gun1.position, transform.rotation);
+        GameObject laser2 = Instantiate(laserBullet, gun2.position, transform.rotation);
+        laser1.transform.Rotate(0, 0, -90);
+        laser2.transform.Rotate(0, 0, -90);
+    }
+
+    private void SetShootToTrue()
+    {
+        shoot = true;
+    }
+}
