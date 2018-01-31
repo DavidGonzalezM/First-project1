21d043ca4ca3ead3a0c75193f085a22dbff4d666
maikbaens <bmiquel@hotmail.es>
2018-01-31 12:50:16 +0100

    subida

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+	[SerializeField]
+	float _speed = 300;
+
+	void Update () {
+		transform.Rotate(new Vector3(0,0,_speed * Time.deltaTime));
+	}
+}
