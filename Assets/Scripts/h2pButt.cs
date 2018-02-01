73eb17cd86a688b36bcf4333f7fe1d3c012fafec
OODDer <35957751+OODDer@users.noreply.github.com>
2018-02-01 13:38:34 +0100

    menu finiquitao

---
+++ Assets/Scripts/h2pButt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class h2pButt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Showme () {
+		SceneManager.LoadScene (2);
+	}
+}
