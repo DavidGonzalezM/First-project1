8c1a7370c6f1bfcf3e4c6562fcc570399672d83a
DavidGonzalezM <dg.martos@gmail.com>
2018-02-01 11:27:00 +0100

    Controller things

---
+++ Assets/Scripts/EndController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndController : MonoBehaviour
+{
+
+    public GameController gc;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gc.EndGame();
+        }
+    }
+}
