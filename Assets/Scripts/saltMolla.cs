96e2ddaade5772a32da771f7a7879eb7477fbfd2
OODDer <35957751+OODDer@users.noreply.github.com>
2018-02-01 11:25:13 +0100

    0.0
    mapa 1 casi acabat
    tenim boto de start

---
+++ Assets/Scripts/saltMolla.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saltMolla : MonoBehaviour {
+	private Rigidbody _rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter (Collision col) {
+		if (col.collider.tag == "molla")
+			_rb.AddForce (0, 200, 0); 
+	}
+}
