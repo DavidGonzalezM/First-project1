82685211fc1851876c4302c69e0ea0ad634ee206
vylion <volfaria@gmail.com>
2018-02-02 11:03:57 +0100

    Button scripts

---
+++ Assets/Scripts/EndPathPuzzle/ButtonActivation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivation : MonoBehaviour {
+
+	public GameObject playerModel;
+	public GameObject pathToReveal;
+
+	private bool activated;
+
+	// Use this for initialization
+	void Start () {
+		activated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance (transform.position, playerModel.transform.position);
+
+		if (dist < 4 && facingButton()) {
+			if (Input.GetKeyDown (KeyCode.E)) {
+				activated = true;
+				pathToReveal.GetComponent<PathReveal> ().checkReveal ();
+			}
+		}
+	}
+
+	bool facingButton() {
+		Vector3 dir = (transform.position - playerModel.transform.position).normalized;
+		float dotProd = Vector3.Dot (dir, playerModel.transform.forward);
+
+		return dotProd > 0.6;
+	}
+
+	public bool isActivated() {
+		return activated;
+	}
+}
---
+++ Assets/Scripts/EndPathPuzzle/PathReveal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathReveal : MonoBehaviour
+{
+	public GameObject[] buttons;
+
+	private bool reveal;
+
+	// Use this for initialization
+	void Start ()
+	{
+		reveal = false;
+
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (reveal)
+			gameObject.SetActive (true);
+	}
+
+	public bool checkReveal() {
+		foreach (GameObject button in buttons) {
+			if (!button.GetComponent<ButtonActivation> ().isActivated ())
+				return false;
+		}
+
+		return true;
+	}
+}
+
